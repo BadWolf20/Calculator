d1a128971246c2abd0192048929a6aa497716f30
BadWolf20 <Roman.Pichugin20@gmail.com>
2021-12-06 15:58:10 +0700

    Prep complete

--- Сalculator/ViewController.swift
+++ Сalculator/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Components
+
+
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupHierarchy()
+        setupLayout()
+        setupView()
     }
 
+    // MARK: - Settings
+    private func setupHierarchy() {
 
+    }
+
+    private func setupLayout() {
+
+
+    }
+
+    private func setupView() {
+        view.backgroundColor = .black
+
+    }
 }
 
+// MARK: - Constants
+
+extension ViewController{
+    enum Colors {
+        static let timeLableFontColor: UIColor = .white
+
+    }
+
+    enum Metric {
+        static let timeLabelFontSize: CGFloat = 80
+
+
+    }
+
+    enum Strings {
+
+    }
+}
